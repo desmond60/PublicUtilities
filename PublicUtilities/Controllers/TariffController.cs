95044e73088fac003e4a07efe02d9cdff797b440
Artyuxov Roman <artyuxov169@gmail.com>
2025-08-29 13:23:49 +0500

    Update TariffController.cs

--- PublicUtilities/Controllers/TariffController.cs
+++ PublicUtilities/Controllers/TariffController.cs
@@ -15,29 +15,29 @@ public class TariffController : ControllerBase
         _tariffService = tariffService;
     }
 
-    [HttpPost("createTariff")]
-    [SwaggerOperation(Summary = "Создание тарифа")]
-    [SwaggerResponse(StatusCodes.Status200OK, "Успешный ответ, созданный тариф", typeof(CreateTariffResultDto))]
-    [SwaggerResponse(StatusCodes.Status400BadRequest, "Не получилось создать тариф")]
-    public async Task<ActionResult<CreateTariffResultDto>> CreateTariff(CreateTariffDto tariff)
-    {
-        var result = await _tariffService.CreateTariff(tariff);
-        if (!result.IsAdd) return BadRequest(result);
+    //[HttpPost("createTariff")]
+    //[SwaggerOperation(Summary = "Создание тарифа")]
+    //[SwaggerResponse(StatusCodes.Status200OK, "Успешный ответ, созданный тариф", typeof(CreateTariffResultDto))]
+    //[SwaggerResponse(StatusCodes.Status400BadRequest, "Не получилось создать тариф")]
+    //public async Task<ActionResult<CreateTariffResultDto>> CreateTariff(CreateTariffDto tariff)
+    //{
+    //    var result = await _tariffService.CreateTariff(tariff);
+    //    if (!result.IsAdd) return BadRequest(result);
 
-        return Ok(result);
-    }
+    //    return Ok(result);
+    //}
 
-    [HttpPost("deleteTariff")]
-    [SwaggerOperation(Summary = "Удаление тарифа")]
-    [SwaggerResponse(StatusCodes.Status200OK, "Успешный ответ, удаленный тариф", typeof(DeleteTariffResultDto))]
-    [SwaggerResponse(StatusCodes.Status400BadRequest, "Не получилось удалить тариф")]
-    public async Task<ActionResult<DeleteTariffResultDto>> DeleteTariff(DeleteTariffDto tariff)
-    {
-        var result = await _tariffService.DeleteTariff(tariff);
-        if (!result.IsDeleted) return BadRequest(result);
+    //[HttpPost("deleteTariff")]
+    //[SwaggerOperation(Summary = "Удаление тарифа")]
+    //[SwaggerResponse(StatusCodes.Status200OK, "Успешный ответ, удаленный тариф", typeof(DeleteTariffResultDto))]
+    //[SwaggerResponse(StatusCodes.Status400BadRequest, "Не получилось удалить тариф")]
+    //public async Task<ActionResult<DeleteTariffResultDto>> DeleteTariff(DeleteTariffDto tariff)
+    //{
+    //    var result = await _tariffService.DeleteTariff(tariff);
+    //    if (!result.IsDeleted) return BadRequest(result);
 
-        return Ok(result);
-    }
+    //    return Ok(result);
+    //}
 
     [HttpGet("allTariff")]
     [SwaggerOperation(Summary = "Получение всех тарифов")]
